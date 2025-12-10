6ea9facdab58b6dea029d0224aed4a6af7381f47
Dave Bevan <dave.bevan@evobytes.co.uk>
2025-12-10 12:16:58 +0000

    Allow install on any release >= 4.2.0

--- netbox_proxbox/__init__.py
+++ netbox_proxbox/__init__.py
@@ -5,11 +5,11 @@ class ProxboxConfig(PluginConfig):
     name = "netbox_proxbox"
     verbose_name = "Proxbox"
     description = "Integrates Proxmox and Netbox"
-    version = "0.0.6b2"
+    version = "0.0.6b2.post2"
     author = "Emerson Felipe (@emersonfelipesp)"
     author_email = "emersonfelipe.2003@gmail.com"
     min_version = '4.2.0'
-    max_version = '4.4.9'
+    max_version = '4.99.99'
     base_url = "proxbox"
     required_settings = []
 
